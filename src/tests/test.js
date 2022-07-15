c0b539ab80d6ce4e28e6b79e78cf391f362c21a1
Shepard <Crazyhistorian2000@yandex.ru>
2022-07-15 15:30:47 +0300

    first test

---
+++ src/tests/test.js
@@ -0,0 +1,10 @@
+
+let firstTest = () => {
+    let user = {     // объект
+        name: "John",  // под ключом "name" хранится значение "John"
+        age: 30        // под ключом "age" хранится значение 30
+    };
+    console.log(user.name)
+}
+
+export default firstTest;
